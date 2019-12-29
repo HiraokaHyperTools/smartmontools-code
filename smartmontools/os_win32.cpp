HiraokaHyperTools/smartmontools-code

commit 77378fb1fa51689ae847b9e3ee1d094078f24f9e

os_win32.cpp: CSMI: Detect missing ATA output registers.
git-svn-id: https://svn.code.sf.net/p/smartmontools/code/trunk@5010 4ea69e1a-61f1-4043-bf83-b5c94c648137

smartmontools/os_win32.cpp
@@ -2353,7 +2353,11 @@ bool csmi_ata_device::ata_pass_through(const ata_cmd_in & in, ata_cmd_out & out)
   }
 
   // Get device-to-host FIS
-  {
+  // Assume values are unavailable if SMART command returns
+  // LBA_MID/HIGH as zero (AMD RAID driver)
+  if (!(   in.in_regs.command == ATA_SMART_CMD
+        && !pthru_buf->Status.bStatusFIS[5]
+        && !pthru_buf->Status.bStatusFIS[6]   )) {
     const unsigned char * fis = pthru_buf->Status.bStatusFIS;
     ata_out_regs & lo = out.out_regs;
     lo.status       = fis[ 2];

commit 22e6ac2e190b14469b57538267df3f1041a75905

os_win32.cpp: CSMI: Check all output registers to fix SCT ERC request (regression from r5010).
git-svn-id: https://svn.code.sf.net/p/smartmontools/code/trunk@5021 4ea69e1a-61f1-4043-bf83-b5c94c648137

smartmontools/os_win32.cpp
@@ -2353,11 +2353,8 @@ bool csmi_ata_device::ata_pass_through(const ata_cmd_in & in, ata_cmd_out & out)
   }
 
   // Get device-to-host FIS
-  // Assume values are unavailable if SMART command returns
-  // LBA_MID/HIGH as zero (AMD RAID driver)
-  if (!(   in.in_regs.command == ATA_SMART_CMD
-        && !pthru_buf->Status.bStatusFIS[5]
-        && !pthru_buf->Status.bStatusFIS[6]   )) {
+  // Assume values are unavailable if all register fields are zero (AMD RAID driver)
+  if (nonempty(pthru_buf->Status.bStatusFIS + 2, 13 - 2 + 1)) {
     const unsigned char * fis = pthru_buf->Status.bStatusFIS;
     ata_out_regs & lo = out.out_regs;
     lo.status       = fis[ 2];